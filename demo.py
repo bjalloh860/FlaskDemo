c9befcfb01853ed44063447037f70a7488908353
Bailor Jalloh <bjalloh@conncoll.edu>
2021-07-13 01:26:28 +0000

    UserRegistration

--- demo.py
+++ demo.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from flask_sqlalchemy import SQLAlchemy
 
-# @app.route("/")                          # this tells you the URL the method below is related to
+
+app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
+app.config['SECRET_KEY']	= '11d81875f26d69dae61e100ee117e8ca'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+	
+	
+@app.route("/")                          # this tells you the URL the method below is related to
 # def hello_world():
 #     return "<p>Hello, World!</p>"        # this prints HTML to the webpage
   
-	
-@app.route("/")
+
+
+@app.route("/home")
 def home():
 	return render_template('home.html', subtitle='Home Page')
 
@@ -15,7 +24,28 @@ def about():
 	return render_template('about.html', subtitle='About Pages',
 						text='This is the about page')
 
-
 	
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+#     global form
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home')) # if so - send to home page
+    return render_template('register.html', title='Register', form=form)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"							
+								
 if __name__ == '__main__':               # this should always be at the end
     app.run(debug=True, host="0.0.0.0")
